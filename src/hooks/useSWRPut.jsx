e4a273dbf6dd23fe9df514dd719497c8ca6dcee8
Wei Ting Wu <weiting@projectmars.info>
2023-09-24 22:50:10 +0800

    fix: add json content header

--- src/hooks/useSWRPut.jsx
+++ src/hooks/useSWRPut.jsx
@@ -1,17 +1,20 @@
 /* eslint-disable no-shadow */
-import useSWRMutation from "swr/mutation";
+import useSWRMutation from 'swr/mutation'
 
 async function sendRequest(url, { arg }) {
-  return fetch(url, {
-    method: "PUT",
-    body: JSON.stringify(arg)
-  }).then((res) => res.json());
+	return fetch(url, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(arg),
+	}).then((res) => res.json())
 }
 
 export default (url) => {
-  const { trigger, isMutating } = useSWRMutation(
-    url,
-    sendRequest /* options */
-  );
-  return { trigger, isMutating };
-};
+	const { trigger, isMutating } = useSWRMutation(
+		url,
+		sendRequest /* options */
+	)
+	return { trigger, isMutating }
+}

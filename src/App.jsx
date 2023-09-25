0915bc3cfbf0c2fd932be714873a0b2a455a4b39
Wei Ting Wu <weiting@projectmars.info>
2023-09-26 03:08:28 +0800

    feat(auth): add clerk auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,65 @@
-import { useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import {
+	ClerkProvider,
+	SignedIn,
+	SignedOut,
+	RedirectToSignIn,
+	SignIn,
+	SignUp,
+	UserButton,
+} from '@clerk/clerk-react'
 
-import "./css/style.css";
+import './css/style.css'
 
-import "./charts/ChartjsConfig";
+import './charts/ChartjsConfig'
 
 // Import pages
-import Dashboard from "./pages/Dashboard";
-import Chat from "./pages/Chat";
+import Dashboard from './pages/Dashboard'
+import Chat from './pages/Chat'
 
-function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    document.querySelector("html").style.scrollBehavior = "auto";
-    window.scroll({ top: 0 });
-    document.querySelector("html").style.scrollBehavior = "";
-  }, [location.pathname]); // triggered on route change
-
-  return (
-    <>
-      <Routes>
-        <Route exact path="/" element={<Dashboard />} />
-        <Route exact path="/chat" element={<Chat />} />
-      </Routes>
-    </>
-  );
+if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
+	throw new Error('Missing Publishable Key')
 }
 
-export default App;
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+function App() {
+	const location = useLocation()
+
+	useEffect(() => {
+		document.querySelector('html').style.scrollBehavior = 'auto'
+		window.scroll({ top: 0 })
+		document.querySelector('html').style.scrollBehavior = ''
+	}, [location.pathname]) // triggered on route change
+
+	return (
+		<ClerkProvider publishableKey={clerkPubKey}>
+			<Routes>
+				<Route exact path="/" element={<Dashboard />} />
+				<Route
+					path="/sign-in/*"
+					element={<SignIn routing="path" path="/sign-in" />}
+				/>
+				<Route
+					path="/sign-up/*"
+					element={<SignUp routing="path" path="/sign-up" />}
+				/>
+				<Route
+					path="/chat"
+					element={
+						<>
+							<SignedIn>
+								<Chat />
+							</SignedIn>
+							<SignedOut>
+								<RedirectToSignIn />
+							</SignedOut>
+						</>
+					}
+				/>
+			</Routes>
+		</ClerkProvider>
+	)
+}
+
+export default App
